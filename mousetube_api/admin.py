1ecc33208c111b720a2659dbfa61313e7df16f6e
lbouri <laurent.bouri2@gmail.com>
2025-05-05 11:40:31 +0200

    add tables in admin interface

--- mousetube_api/admin.py
+++ mousetube_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Strain, Subject, Protocol, Experiment, File
+from .models import User, Strain, Subject, Protocol, Experiment, File, Software, Reference
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -73,6 +73,37 @@ class FileAdmin(admin.ModelAdmin):
     )
     list_filter = ("is_valid_link", "experiment", "subject")
 
+class SoftwareAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "type",
+        "made_by",
+        "description",
+        "technical_requirements",
+    )
+    search_fields = (
+        "name",
+        "type",
+        "made_by",
+        "description",
+        "technical_requirements",
+    )
+    filter_horizontal = ("references", "users")
+
+class ReferenceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "url",
+        "doi",
+    )
+    search_fields = (
+        "name",
+        "description",
+        "url",
+        "doi",
+    )
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Strain, StrainAdmin)
@@ -80,3 +111,5 @@ admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Protocol, ProtocolAdmin)
 admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(File, FileAdmin)
+admin.site.register(Software, SoftwareAdmin)
+admin.site.register(Reference, ReferenceAdmin)
